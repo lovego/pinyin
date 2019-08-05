adebf0599c7b98e341361ed7cfa19e901b7dc1db
bughou <bughou@gmail.com>
2019-08-05 11:57:38 +0800

    save

--- pinyin.go
+++ pinyin.go
@@ -1,4 +1,4 @@
-package utils
+package pinyin
 
 import (
 	"github.com/mozillazg/go-pinyin"
--- pinyin_test.go
+++ pinyin_test.go
@@ -1,11 +1,15 @@
-package utils
+package pinyin_test
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lovego/pinyin"
+)
 
 func ExampleInitials() {
-	fmt.Println(Initials("长大"))
-	fmt.Println(Initials("长城abc"))
-	fmt.Println(Initials(" a长b大,c "))
+	fmt.Println(pinyin.Initials("长大"))
+	fmt.Println(pinyin.Initials("长城abc"))
+	fmt.Println(pinyin.Initials(" a长b大,c "))
 	// Output:
 	// ZD
 	// ZCABC
