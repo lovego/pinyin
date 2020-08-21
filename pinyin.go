1dd570be79ed5121bd661132f7a8ad59241217b0
詹杨洋 <yangyang.zhan@hztl3.com>
2020-08-21 17:35:51 +0800

    修改dict 为map

--- pinyin.go
+++ pinyin.go
@@ -3,6 +3,7 @@ package pinyin
 import (
 	"regexp"
 	"strings"
+	"unicode"
 )
 
 var (
@@ -11,14 +12,14 @@ var (
 
 // 获取所有汉字的首字母组合， 比如"长城ABC"，返回"CCABC"
 // -keepSpecial 保留特殊符号
-func Initials(s string, keepSpecial bool) string {
+func Initials(s string, keepSpecial bool) (res string) {
 	if !keepSpecial {
 		s = filterRe.ReplaceAllString(s, "")
 	}
-	for i := 0; i < len(dict); i += 2 {
-		s = strings.Replace(s, dict[i], dictInitials(dict[i+1]), -1)
+	for _, word := range split(s) {
+		res += word.initials()
 	}
-	return strings.ToUpper(s)
+	return
 }
 
 func dictInitials(s string) string {
@@ -29,4 +30,63 @@ func dictInitials(s string) string {
 		}
 	}
 	return v
+}
+
+type word struct {
+	V       string
+	Chinese bool
+}
+
+func split(s string) (ws []word) {
+	var temp string
+	lastType := 0
+	for _, v := range s {
+		t := unicode.Is(unicode.Han, v)
+		if (t && lastType == 1) || (!t && lastType == -1) || (lastType == 0) {
+			temp += string(v)
+		} else {
+			ws = append(ws, word{
+				V:       temp,
+				Chinese: lastType == 1,
+			})
+			temp = string(v)
+			lastType = 0
+		}
+		if t {
+			lastType = 1
+		} else {
+			lastType = -1
+		}
+	}
+	ws = append(ws, word{
+		V:       temp,
+		Chinese: lastType == 1,
+	})
+	return
+}
+
+func (w word) initials() (res string) {
+	if !w.Chinese {
+		return strings.ToUpper(w.V)
+	}
+	chinese := []rune(w.V)
+	vL := len(chinese)
+	for i := 0; i < vL; {
+		var j int
+		var contain bool
+		for ; j <= vL-i-1; j++ {
+			if py, ok := dictMap[string(chinese[i:vL-j])]; ok {
+				res += dictInitials(py)
+				contain = true
+				break
+			}
+		}
+		if !contain {
+			res += string(chinese[i : vL-j+1])
+			i++
+		} else {
+			i += vL - i - j
+		}
+	}
+	return
 }
